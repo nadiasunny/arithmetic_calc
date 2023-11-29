d87bdd448644245472e30dc50d3185c022006f93
Nadia Hina <nadianadia062@gmail.com>
2023-11-29 12:28:15 -0800

    Added powers

--- arithmetic.py
+++ arithmetic.py
@@ -35,7 +35,8 @@ def cube(num1):
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
-
+    powered = num1 ** num2
+    return powered
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
