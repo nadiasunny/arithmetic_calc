6712b67c32ea0c41978df6c5482b789cf4b4212b
Nadia Hina <nadianadia062@gmail.com>
2023-11-29 12:16:54 -0800

    Added subtraction function

--- arithmetic.py
+++ arithmetic.py
@@ -9,8 +9,8 @@ def add(num1, num2):
 
 def subtract(num1, num2):
     """Return the value of num1 minus num2."""
-    
-    return
+    sub = num1 - num2
+    return sub
 
 def multiply(num1, num2):
     """Multiply the num1 by num2 and return the result."""
